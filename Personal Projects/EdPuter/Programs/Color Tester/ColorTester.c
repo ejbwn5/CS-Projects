58c7262965e0f2dc9c941b3511678b3ea0854f3e
ejbwn5 <ed-bateman@att.net>
2021-12-09 05:19:37 -0600

    Updated code

--- Personal Projects/EdPuter/Programs/Color Tester/ColorTester.c
+++ Personal Projects/EdPuter/Programs/Color Tester/ColorTester.c
@@ -1,40 +1,60 @@
-
-
-
-
-
-
-
-
-
-
 int offset;
-
+int offset2;
+int offset3;
+int timer;
 
 int main()
 {
+  //initialize variables
   offset = 0;
+  offset2 = 0;
+  offset3 = 0;
+  timer = 0;
   
-  while (1)
+  while (1) //loop forever
   {
     
-    draw();
-    flip;
-    offset = offset + 1;
+    if (timer < 10) //draw the first pattern for 10 frames
+    {
+      draw1();
+    }
+    else
+    {
+      if (timer < 20) //then 10 frames of the second pattern
+      {
+        draw2();
+      }
+      else
+      {
+       if (timer < 30) //then third
+       {
+         draw3();
+       }
+       else //then fourth
+       {
+         draw4();
+       } 
+      }
+    }
+    
+    
+    flip; //flip buffer after drawing
+    offset = offset + 1; //increment offset and timer
+    timer = timer + 1;    
+    if (timer > 39) //loop back around
+    {
+      timer = 0;
+    }    
   }  
   return 0;
 }
 
-
-
-
 int x, y;
 int addr;
+int col;
 
-
-void drawDiag()
-{
-  
+void draw1() //pattern 1
+{  
   y = 0;
   addr = 0;
   
@@ -42,33 +62,85 @@ void drawDiag()
   {    
     x = 0;
     while (x < 100)
-    {
-      
+    {      
       vrammar(addr);
       write(x + y + offset);
-      
-      
       addr = addr + 1;
       x = x + 1;
     }
     addr = addr + 28;
     y = y + 1;
   }
+  return void;
+}
+
+void draw2()
+{  
+  y = 0;
+  addr = 0;
+  col = 0;
   
+  while (y < 75)
+  {    
+    x = 0;
+    while (x < 100)
+    {      
+      vrammar(addr);
+      write(col+offset);
+      addr = addr + 1;
+      x = x + 1;
+    }
+    addr = addr + 28;
+    y = y + 1;
+    col = col + 3;
+  }
+  return void;
+}
+
+void draw3()
+{  
+  y = 0;
+  addr = 0;
   
+  while (y < 75)
+  {    
+    x = 0;
+    while (x < 100)
+    {      
+      vrammar(addr);
+      write(x + x + y + offset2);
+      addr = addr + 1;
+      x = x + 1;
+    }
+    addr = addr + 28;
+    y = y + 1;    
+  }  
+  offset2 = offset2 - 4;
   
   return void;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+void draw4()
+{  
+  y = 0;
+  addr = 0;
+  col = 0;
+  
+  while (y < 75)
+  {    
+    x = 0;
+    while (x < 100)
+    {      
+      vrammar(addr);
+      write(x + offset + col + offset3);
+      addr = addr + 1;
+      x = x + 1;
+    }
+    addr = addr + 28;
+    y = y + 1;
+    col = col + 32;
+  }  
+  offset3 = offset3 + 32;
+  
+  return void;
+}
--- Personal Projects/EdPuter/Programs/Color Tester/Output.asm
+++ Personal Projects/EdPuter/Programs/Color Tester/Output.asm
@@ -3,6 +3,12 @@ end:  JMP end
 mainAdd:  .DA main
 main: LDAI 0
 STA $offset
+LDAI 0
+STA $offset2
+LDAI 0
+STA $offset3
+LDAI 0
+STA $timer
 loopstart0: LDAI 1
 PUSHA
 LDAI 0
@@ -10,17 +16,61 @@ MOVAB
 POPA
 CMP
 JZI loopdn0
-CALL draw
-FLIP
+LDARAM $timer
+PUSHA
+LDAI 10
+MOVAB
+POPA
+CMP
+JCI ifcnt2
+JMP ifskp2
+ifcnt2: CALL draw1
+JMP if2skp2
+ifskp2: LDARAM $timer
+PUSHA
+LDAI 20
+MOVAB
+POPA
+CMP
+JCI ifcnt1
+JMP ifskp1
+ifcnt1: CALL draw2
+JMP if2skp1
+ifskp1: LDARAM $timer
+PUSHA
+LDAI 30
+MOVAB
+POPA
+CMP
+JCI ifcnt0
+JMP ifskp0
+ifcnt0: CALL draw3
+JMP if2skp0
+ifskp0: CALL draw4
+if2skp0: if2skp1: if2skp2: FLIP
 LDARAM $offset
 LDBI 1
 ADDA
 STA $offset
-JMP loopstart0
+LDARAM $timer
+LDBI 1
+ADDA
+STA $timer
+LDAI 39
+PUSHA
+LDARAM $timer
+MOVAB
+POPA
+CMP
+JCI ifcnt3
+JMP ifskp3
+ifcnt3: LDAI 0
+STA $timer
+ifskp3: JMP loopstart0
 loopdn0: LDAI 0
 RET
 RET
-draw: LDAI 0
+draw1: LDAI 0
 STA $y
 LDAI 0
 STA $addr
@@ -70,4 +120,179 @@ ADDA
 STA $y
 JMP loopstart2
 loopdn2: RET
+RET
+draw2: LDAI 0
+STA $y
+LDAI 0
+STA $addr
+LDAI 0
+STA $col
+loopstart4: LDARAM $y
+PUSHA
+LDAI 75
+MOVAB
+POPA
+CMP
+JCI loopcnt4
+JMP loopdn4
+loopcnt4: LDAI 0
+STA $x
+loopstart3: LDARAM $x
+PUSHA
+LDAI 100
+MOVAB
+POPA
+CMP
+JCI loopcnt3
+JMP loopdn3
+loopcnt3: LDARAM $addr
+VMARA
+LDARAM $col
+LDBRAM $offset
+ADDA
+MOVAC
+VRCC
+LDARAM $addr
+LDBI 1
+ADDA
+STA $addr
+LDARAM $x
+LDBI 1
+ADDA
+STA $x
+JMP loopstart3
+loopdn3: LDARAM $addr
+LDBI 28
+ADDA
+STA $addr
+LDARAM $y
+LDBI 1
+ADDA
+STA $y
+LDARAM $col
+LDBI 3
+ADDA
+STA $col
+JMP loopstart4
+loopdn4: RET
+RET
+draw3: LDAI 0
+STA $y
+LDAI 0
+STA $addr
+loopstart6: LDARAM $y
+PUSHA
+LDAI 75
+MOVAB
+POPA
+CMP
+JCI loopcnt6
+JMP loopdn6
+loopcnt6: LDAI 0
+STA $x
+loopstart5: LDARAM $x
+PUSHA
+LDAI 100
+MOVAB
+POPA
+CMP
+JCI loopcnt5
+JMP loopdn5
+loopcnt5: LDARAM $addr
+VMARA
+LDARAM $x
+LDBRAM $x
+ADDA
+LDBRAM $y
+ADDA
+LDBRAM $offset2
+ADDA
+MOVAC
+VRCC
+LDARAM $addr
+LDBI 1
+ADDA
+STA $addr
+LDARAM $x
+LDBI 1
+ADDA
+STA $x
+JMP loopstart5
+loopdn5: LDARAM $addr
+LDBI 28
+ADDA
+STA $addr
+LDARAM $y
+LDBI 1
+ADDA
+STA $y
+JMP loopstart6
+loopdn6: LDARAM $offset2
+LDBI 4
+SUBA
+STA $offset2
+RET
+RET
+draw4: LDAI 0
+STA $y
+LDAI 0
+STA $addr
+LDAI 0
+STA $col
+loopstart8: LDARAM $y
+PUSHA
+LDAI 75
+MOVAB
+POPA
+CMP
+JCI loopcnt8
+JMP loopdn8
+loopcnt8: LDAI 0
+STA $x
+loopstart7: LDARAM $x
+PUSHA
+LDAI 100
+MOVAB
+POPA
+CMP
+JCI loopcnt7
+JMP loopdn7
+loopcnt7: LDARAM $addr
+VMARA
+LDARAM $x
+LDBRAM $offset
+ADDA
+LDBRAM $col
+ADDA
+LDBRAM $offset3
+ADDA
+MOVAC
+VRCC
+LDARAM $addr
+LDBI 1
+ADDA
+STA $addr
+LDARAM $x
+LDBI 1
+ADDA
+STA $x
+JMP loopstart7
+loopdn7: LDARAM $addr
+LDBI 28
+ADDA
+STA $addr
+LDARAM $y
+LDBI 1
+ADDA
+STA $y
+LDARAM $col
+LDBI 32
+ADDA
+STA $col
+JMP loopstart8
+loopdn8: LDARAM $offset3
+LDBI 32
+ADDA
+STA $offset3
+RET
 RET
